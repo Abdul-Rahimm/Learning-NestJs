eae92393b12b7c85b11f68738c729a4b650180d4
Abdul Rahim <k213951@nu.edu.pk>
2025-06-30 12:35:42 +0500

    created the schema with hydrated document

---
+++ uber-services/apps/logging/src/rider-coordinates/schemas/rider-coordinates.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+// latitude, longitude and id of rider
+export type RiderCoordinatesDocument = HydratedDocument<RiderCoordinates>;
+
+@Schema()
+class RiderCoordinates {
+    @Prop({ required: true, unique: true })
+    riderId: string;
+
+    @Prop({ required: true })
+    lat: number;
+
+    @Prop({ required: true })
+    lon: number;
+}
+
+export const RiderCoordinatesSchema = SchemaFactory.createForClass(RiderCoordinates);
